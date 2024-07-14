8e041ad77a1c181e8e31a69e06513a73db843027
leonardo rodrigues <leonardorodrigues_f@hotmail.com>
2024-07-14 20:33:48 -0300

    fix: ajustando o path onde serão baixadas as dependências

--- app/local.py
+++ app/local.py
@@ -1,10 +1,10 @@
-# from lambda_function import lambda_handler
+from lambda_function import lambda_handler
 
-# payload = {
-#     'Records': [
-#         {
-#             'body': "{\"value\": 100,\"register\": true,\"productsIds\": [\"19e61598-3147-49e3-a42a-def44390846c\"],\"client\": {\"cpf\": \"23144234565\",\"name\": \"nome completo\",\"email\": \"nome@completo.com\"}}"
-#         }
-#     ]
-# }
-# lambda_handler(payload, None)
+payload = {
+    'Records': [
+        {
+            'body': "{\"value\": 100,\"register\": true,\"productsIds\": [\"19e61598-3147-49e3-a42a-def44390846c\"],\"client\": {\"cpf\": \"23144234565\",\"name\": \"nome completo\",\"email\": \"nome@completo.com\"}}"
+        }
+    ]
+}
+lambda_handler(payload, None)
